aa4bdd833b916e832c5ce01ec095369c192d5baa
Aditi Gupta <aditigupta385@gmail.com>
2023-08-15 20:50:12 +0530

    Time: 4 ms (78.23%) | Memory: 10.3 MB (21.22%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -1,23 +1,32 @@
 class Solution {
  public:
-  ListNode* partition(ListNode* head, int x) {
-    ListNode beforeHead(0);
-    ListNode afterHead(0);
-    ListNode* before = &beforeHead;
-    ListNode* after = &afterHead;
+ ListNode* partition(ListNode* head, int x) {
+    ListNode* temp = head  ; 
 
-    for (; head; head = head->next)
-      if (head->val < x) {
-        before->next = head;
-        before = head;
-      } else {
-        after->next = head;
-        after = head;
-      }
+    //Creating 2 LinkedLists 
+    ListNode* left = new ListNode(0);
+    ListNode* right = new ListNode(0) ; 
 
-    after->next = nullptr;
-    before->next = afterHead.next;
+    //using 2 nodes to point head
+    ListNode* dummy1 = left  ;
+    ListNode* dummy2 = right ; 
 
-    return beforeHead.next;
-  };
+    while(temp!=NULL){
+        //if the value at temp is less than x, we've stored the value in dummy 1, and stored in dummy2 if temp is greater than x;
+        if(temp->val < x){
+            dummy1->next = temp ; 
+            dummy1 = dummy1->next ;     
+         }
+        else {
+            dummy2->next = temp ; 
+            dummy2 = dummy2->next ; 
+            
+        }
+        temp = temp->next ; 
+    }
+    dummy1->next = right->next ; 
+    dummy2->next = NULL;
+    
+    return left->next ; 
+}
 };
